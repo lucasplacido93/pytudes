7d97847b25a0a217aea07c88e860f5257e2de954
lucas placido <lucas.placido.93@gmail.com>
2020-08-11 20:33:46 -0300

    feat: aula 03 testando valores

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,1 +1,18 @@
-print("Bem vindo no jogo de Advinhação!")
+print("*********************************")
+print("Bem vindo no jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu numero: ")
+print("Você digitou ", chute_str)
+chute = int (chute_str)
+
+if(numero_secreto == chute):
+    print("Você acertou!")
+else:
+    if(chute > numero_secreto):
+        print("Você errou! O Seu chute foi maior do que o número secreto.")
+    elif(chute < numero_secreto):
+        print("Você errou! O Seu chute foi menor do que o número secreto.")
+print("Fim do jogo")
