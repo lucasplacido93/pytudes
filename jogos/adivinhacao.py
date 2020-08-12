11de1cbac5f2244ffcbf57b9807a799f1b36b987
lucas placido <lucas.placido.93@gmail.com>
2020-08-12 19:42:49 -0300

    feat: adicionando acertou na variavel

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -8,7 +8,11 @@ chute_str = input("Digite o seu numero: ")
 print("Você digitou ", chute_str)
 chute = int (chute_str)
 
-if(numero_secreto == chute):
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if(acertou):
     print("Você acertou!")
 else:
     if(chute > numero_secreto):
